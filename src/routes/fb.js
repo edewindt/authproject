d6cb3f25012dcb819577f1dd1bf3db31c2a716f5
edewindt <edewindt27@outlook.com>
2022-07-21 16:16:55 -0500

    added netlify config

---
+++ src/routes/fb.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyBdMiZSamfwEw0VX0tM69eeqvrqPoGMHdI',
+	authDomain: 'test-61c44.firebaseapp.com',
+	projectId: 'test-61c44',
+	storageBucket: 'test-61c44.appspot.com',
+	messagingSenderId: '855995772287',
+	appId: '1:855995772287:web:85883b9fa6dc6c27f43e4a',
+	measurementId: 'G-7QJPQHMW4E'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
